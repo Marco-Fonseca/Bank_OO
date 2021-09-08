eea0d848aba04c7690a15ce368364ac1ba938992
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-09-08 09:43:12 -0300

    Add dos arquivos de cod criados para o projeto.

---
+++ myBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ContaCorrente contaDaBarbara = new ContaCorrente();
+
+            contaDaBarbara.titular = "Bárbara";
+            contaDaBarbara.agencia = 1234;
+            contaDaBarbara.conta = 789;
+            contaDaBarbara.saldo = 100.50;
+
+            ContaCorrente contaDoJoao = new ContaCorrente();
+
+            contaDoJoao.titular = "João";
+            contaDoJoao.agencia = 1234;
+            contaDoJoao.conta = 790;
+            contaDoJoao.saldo = 120;
+
+            Console.WriteLine("O titular da conta é:" + contaDaBarbara.titular);
+            Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
+        }
+    }
+}
