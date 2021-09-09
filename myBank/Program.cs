1791a23bb4b47612108f2d3422cbc57ebb666458
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-09-09 09:37:46 -0300

    Add de cod atualizado com construtor

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -8,22 +8,14 @@ namespace myBank
         static void Main(string[] args)
         {
             
-            ContaCorrente contaDaBarbara = new ContaCorrente();
+            ContaCorrente contaDaBarbara = new ContaCorrente("Bárbara",1234,789,100.50);
 
-            contaDaBarbara.titular = "Bárbara";
-            contaDaBarbara.agencia = 1234;
-            contaDaBarbara.conta = 789;
-            contaDaBarbara.saldo = 100.50;
+            ContaCorrente contaDoJoao = new ContaCorrente("João",1234,790,120);
 
-            ContaCorrente contaDoJoao = new ContaCorrente();
-
-            contaDoJoao.titular = "João";
-            contaDoJoao.agencia = 1234;
-            contaDoJoao.conta = 790;
-            contaDoJoao.saldo = 120;
-
+            
             Console.WriteLine("O titular da conta é:" + contaDaBarbara.titular);
             Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
+            
         }
     }
 }
