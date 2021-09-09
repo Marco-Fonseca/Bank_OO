e34e18fec19166af870b1319c0067c22f474b941
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-09-09 09:41:07 -0300

    Troca de nome do titular da conta

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -8,12 +8,12 @@ namespace myBank
         static void Main(string[] args)
         {
             
-            ContaCorrente contaDaBarbara = new ContaCorrente("Bárbara",1234,789,100.50);
+            ContaCorrente contaDoMarco = new ContaCorrente("Marco",1234,789,100.50);
 
             ContaCorrente contaDoJoao = new ContaCorrente("João",1234,790,120);
 
             
-            Console.WriteLine("O titular da conta é:" + contaDaBarbara.titular);
+            Console.WriteLine("O titular da conta é:" + contaDoMarco.titular);
             Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
             
         }
