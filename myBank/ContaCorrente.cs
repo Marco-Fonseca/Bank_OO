0df146732fd59bf2fc8eeee60abb645820759807
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-09-21 19:53:59 -0300

    Add dos métodos da conta corrente

--- myBank/ContaCorrente.cs
+++ myBank/ContaCorrente.cs
@@ -16,4 +16,36 @@ public class ContaCorrente
         this.saldo = saldo;
     }
 
+
+    public bool Sacar(double valor)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }   
+        else 
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Trasnferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.Sacar(valor);
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -9,13 +9,20 @@ namespace myBank
         {
             
             ContaCorrente contaDoMarco = new ContaCorrente("Marco",1234,789,100.50);
-
             ContaCorrente contaDoJoao = new ContaCorrente("João",1234,790,120);
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria",1234,791,150);
 
-            
-            Console.WriteLine("O titular da conta é:" + contaDoMarco.titular);
-            Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
-            
+            bool retorno = contaDoMarco.Sacar(50);
+            Console.WriteLine(retorno);
+            Console.WriteLine("O saldo da conta do Marco é:R$"+ contaDoMarco.saldo);
+           
+
+            contaDoMarco.Depositar(150);
+            Console.WriteLine("O saldo da conta do Marco é:R$"+ contaDoMarco.saldo);
+
+            contaDoMarco.Trasnferir(200,contaDoJoao);
+            Console.WriteLine("O saldo da conta do Marco é:R$"+ contaDoMarco.saldo);
+            Console.WriteLine("O saldo da conta do João é:R$"+ contaDoJoao.saldo);
         }
     }
 }
