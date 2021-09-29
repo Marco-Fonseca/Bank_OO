75598856d6718ae1359040b34e78e27fadf665ae
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-09-29 09:51:18 -0300

    Add de consoles de saída para validação das operações

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -29,28 +29,28 @@ namespace myBank
             Console.WriteLine("Funcionário 2 é o:"+ funcionarioJorge.Nome);
             Console.WriteLine("----------------------------------------------------");
            
+           Console.WriteLine("O saldo da conta do Marco é R$"+ contaDoMarco.Saldo);
+           Console.WriteLine("Sacando R$50,00");
            contaDoMarco.Sacar(50);
            Console.WriteLine("O saldo da conta do Marco é R$"+ contaDoMarco.Saldo);
            Console.WriteLine("----------------------------------------------------");
 
+           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
+           Console.WriteLine("Depositando R$300,00");
            contaDoGustavo.Depositar(300);
            Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
            Console.WriteLine("----------------------------------------------------");
 
+
+           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
+           Console.WriteLine("O saldo da conta da Márcia é R$"+ contaDaMarcia.Saldo);
+           Console.WriteLine("Transferindo R$200,00 do Gustavo para a Márcia");
            contaDoGustavo.Trasnferir(200,contaDaMarcia);
            Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
            Console.WriteLine("O saldo da conta da Márcia é R$"+ contaDaMarcia.Saldo);
            Console.WriteLine("----------------------------------------------------");
 
            Console.WriteLine("O número total de contas é:"+ ContaCorrente.TotalContaCriadas);
-
-            /*Console.WriteLine("O saldo da conta do João é:R$"+ contaDoJoao.Saldo);
-            Console.WriteLine("O saldo da conta do Marco é:R$"+ contaDoMarco.Saldo);
-            Console.WriteLine("O número total de contas é:"+ ContaCorrente.TotalContaCriadas);
-            Console.WriteLine("O total de comissão do funcionário é R$"+ ContaCorrente.TotalDeComissao);
-            Console.WriteLine("O total de comissão do funcionário é R$"+ ContaCorrente.TotalDeComissao);
-            Console.WriteLine("A comissão do funcionário Pedro é R$"+ funcionarioPedro.Comissao);
-            Console.WriteLine("A comissão do funcionário Jorge é R$"+ funcionarioJorge.Comissao);*/
         }
     }
 }
