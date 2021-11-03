6621c202bcba7c17585c32bcb83c51a984d978c5
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-11-03 09:17:59 -0300

    Add Remuneração total dos funcionários

--- myBank/Funcionario.cs
+++ myBank/Funcionario.cs
@@ -15,8 +15,20 @@ public abstract class Funcionario
         }
         set
         {
-                _bonificacao += value * 0.01;
+            _bonificacao += Salario * 0.01;
         }
+    }
+
+    public abstract int CalcularFerias();
+
+    public double RemuneracaoTotal(double Salario)
+    {
+        return Salario * 12 + Salario;
+    }
+
+    public double RemuneracaoTotal(double Salario, double Bonificacao)
+    {
+        return Salario * 12 + Salario + Bonificacao;
     }
 
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -11,72 +11,29 @@ namespace myBank
 
                 Nome = "Henrique",
                 CPF = "987.987.987-98",
-                Salario = 2000
+                Salario = 2000 ,
+                NumeroDeAgencias = 4
             };
 
             Vendedor vendedorPedro = new Vendedor()
             {
                 Nome = "Pedro",
                 CPF = "123.123.123-12",
-                Salario = 1000
+                Salario = 1000 ,
+                NumeroClientesAtendidos = 3
             };
             Vendedor vendedorJorge = new Vendedor()
             {
                 Nome = "Jorge",
                 CPF = "321.321.321-21",
-                Salario = 1000
+                Salario = 1000 ,
+                NumeroClientesAtendidos = 5
             };
-
-            vendedorPedro.Bonificacao = vendedorPedro.Salario;
-
-             Console.WriteLine("O salario do gerente Henrique é R$" + gerenteHenrique.Salario);
-             Console.WriteLine("O Bonificação do Pedro é:"+ vendedorPedro.Bonificacao);
-
-           /* ContaCorrente contaDoMarco = new ContaCorrente("Marco",1234,100,funcionarioPedro);
-            contaDoMarco.Conta = 789;
-
-            ContaCorrente contaDoGustavo = new ContaCorrente("Gustavo",1234,120,funcionarioJorge);
-            contaDoGustavo.Conta = 790;
-
-            ContaCorrente contaDaMarcia = new ContaCorrente("Márcia",1234,150,funcionarioPedro);
-            contaDaMarcia.Conta = 791;
-
             
-            Console.WriteLine("O Bonificação do Pedro é:"+ funcionarioPedro.Bonificacao);
-            Console.WriteLine("O Salario do Pedro é:"+ funcionarioPedro.Salario);
+                gerenteHenrique.Bonificacao = gerenteHenrique.Salario;
+        
+             Console.WriteLine("A Remuneração total do gerente Henrique é de R$" + gerenteHenrique.RemuneracaoTotal(gerenteHenrique.Salario,gerenteHenrique.Bonificacao));
 
-
-            Console.WriteLine("A Conta corrente 1 é a do:"+ contaDoMarco.Titular);
-            Console.WriteLine("A Conta corrente 2 é a do:"+ contaDoGustavo.Titular);
-            Console.WriteLine("A Conta corrente 3 é a da:"+ contaDaMarcia.Titular);
-            Console.WriteLine("----------------------------------------------------");
-
-            Console.WriteLine("Funcionário 1 é o:"+ funcionarioPedro.Nome);
-            Console.WriteLine("Funcionário 2 é o:"+ funcionarioJorge.Nome);
-            Console.WriteLine("----------------------------------------------------");
-           
-           Console.WriteLine("O saldo da conta do Marco é R$"+ contaDoMarco.Saldo);
-           Console.WriteLine("Sacando R$50,00");
-           contaDoMarco.Sacar(50);
-           Console.WriteLine("O saldo da conta do Marco é R$"+ contaDoMarco.Saldo);
-           Console.WriteLine("----------------------------------------------------");
-
-           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
-           Console.WriteLine("Depositando R$300,00");
-           contaDoGustavo.Depositar(300);
-           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
-           Console.WriteLine("----------------------------------------------------");
-
-
-           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
-           Console.WriteLine("O saldo da conta da Márcia é R$"+ contaDaMarcia.Saldo);
-           Console.WriteLine("Transferindo R$200,00 do Gustavo para a Márcia");
-           contaDoGustavo.Trasnferir(200,contaDaMarcia);
-           Console.WriteLine("O saldo da conta do Gustavo é R$"+ contaDoGustavo.Saldo);
-           Console.WriteLine("O saldo da conta da Márcia é R$"+ contaDaMarcia.Saldo);
-           Console.WriteLine("----------------------------------------------------");
-
-           Console.WriteLine("O número total de contas é:"+ ContaCorrente.TotalContaCriadas);*/
         }
     }
 }
